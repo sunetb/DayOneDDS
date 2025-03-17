bf16e603c58cd2afa0a5fcefe121cf08e10bf9b0
sunetb <sunetb@gmail.com>
2025-03-17 10:49:36 +0100

    First commit

---
+++ app/src/main/java/com/example/dayonedds/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.dayonedds;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button click;
+    TextView hello;
+    EditText nameInputField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hello = findViewById(R.id.hello);
+        hello.setText("Welcome");
+        nameInputField = findViewById(R.id.editTextText);
+        click = findViewById(R.id.button);
+        click.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //System.out.println("Click: " + hello.getText().toString());
+        //Log.d("my label", "my message"); //Alternative to System.out.println
+        String name = nameInputField.getText().toString();
+        hello.setText("Welcome " + name + "!");
+
+
+    }
+}
